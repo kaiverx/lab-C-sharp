0eefa795d4c8c1ba5c8c7fb8884e058e352a1022
kaiverx <praim200570@gmail.com>
2025-02-24 12:20:04 +0800

    переделка под автотесты

--- lab1/10.cs
+++ lab1/10.cs
@@ -2,17 +2,38 @@
 //а) самое большое; б) самое маленькое; в) является средним (средним назовем число, которое больше
 //наименьшего из данных чисел, но меньше наибольшего).
 
-int[] numbers = new int[3];
-Console.WriteLine("Введите 3 целых числа ");
-
-// Читаем три числа с консоли
-for (int i = 0; i < 3; i++)
+namespace NumberComparison
 {
-    numbers[i] = int.Parse(Console.ReadLine());
+    public class Logic
+    {
+        /**
+         * Метод для определения самого большого, среднего и самого маленького числа
+         */
+        public static (int max, int mid, int min) GetSorted(int first, int second, int third)
+        {
+            int[] numbers = { first, second, third };
+            Array.Sort(numbers);
+            return (numbers[2], numbers[1], numbers[0]);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 целых числа:");
+
+            // Читаем три числа с консоли
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            // Вызываем метод для обработки чисел
+            var (max, mid, min) = Logic.GetSorted(first, second, third);
+
+            // Выводим результат
+            Console.WriteLine($"{max} - Наибольшее число, {mid} - Среднее число, {min} - Наименьшее число");
+            Console.ReadLine();
+        }
+    }
 }
-
-// Сортируем массив
-Array.Sort(numbers);
-
-// Выводим результат
-Console.WriteLine($"{numbers[2]} - Наибольшее число, {numbers[1]} - Среднее число, {numbers[0]} - Наименьшее число");
---
+++ lab1Tests/LogicTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberComparison;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberComparison.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetSortedTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
