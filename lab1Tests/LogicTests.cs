c5b9d9e8f0efe910cf46d7168c29f464d43c8536
kaiverx <praim200570@gmail.com>
2025-02-24 12:31:08 +0800

    авто тесты

--- lab1Tests/LogicTests.cs
+++ lab1Tests/LogicTests.cs
@@ -12,9 +12,63 @@ namespace NumberComparison.Tests
     public class LogicTests
     {
         [TestMethod()]
-        public void GetSortedTest()
+        public void GetSortedTest1()
         {
-            Assert.Fail();
+            // Arrange
+            int first = 5, second = 2, third = 8;
+
+            // Act
+            var (max, mid, min) = Logic.GetSorted(first, second, third);
+
+            // Assert
+            Assert.AreEqual(8, max);
+            Assert.AreEqual(5, mid);
+            Assert.AreEqual(2, min);
+        }
+
+        [TestMethod()]
+        public void GetSortedTest2()
+        {
+            // Arrange
+            int first = 9, second = 12, third = 3;
+
+            // Act
+            var (max, mid, min) = Logic.GetSorted(first, second, third);
+
+            // Assert
+            Assert.AreEqual(12, max);
+            Assert.AreEqual(9, mid);
+            Assert.AreEqual(3, min);
+        }
+
+        [TestMethod()]
+        public void GetSortedTest3()
+        {
+            // Arrange
+            int first = -1, second = -10, third = 0;
+
+            // Act
+            var (max, mid, min) = Logic.GetSorted(first, second, third);
+
+            // Assert
+            Assert.AreEqual(0, max);
+            Assert.AreEqual(-1, mid);
+            Assert.AreEqual(-10, min);
+        }
+
+        [TestMethod()]
+        public void GetSortedTest4()
+        {
+            // Arrange
+            int first = -5, second = -2, third = -8;
+
+            // Act
+            var (max, mid, min) = Logic.GetSorted(first, second, third);
+
+            // Assert
+            Assert.AreEqual(-2, max);
+            Assert.AreEqual(-5, mid);
+            Assert.AreEqual(-8, min);
         }
     }
 }
